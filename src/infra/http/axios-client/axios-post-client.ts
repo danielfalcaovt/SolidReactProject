232ac619ce9cf39c7cbf172e10a1e081f663a559
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-03 15:44:11 -0300

    feat: ensure AxiosPostClient return a httpResponse if axios throws an error

--- src/infra/http/axios-client/axios-post-client.ts
+++ src/infra/http/axios-client/axios-post-client.ts
@@ -10,10 +10,26 @@ export class AxiosHttpClient implements IHttpPostClient<any, any> {
   async post (
     httpClientParams: IHttpPostClientParams<any>
   ): Promise<httpResponse<any>> {
-    await axios.post(httpClientParams.url, httpClientParams.body)
-    return new Promise(resolve => resolve({
-      statusCode: 200,
-      body: []
-    }))
+    try {
+      await axios.post(httpClientParams.url, httpClientParams.body)
+      return new Promise((resolve) =>
+        resolve({
+          statusCode: 200,
+          body: []
+        })
+      )
+    } catch (err: any) {
+      if (err.response) {
+        return new Promise(resolve => resolve({
+          statusCode: err.response.status,
+          body: err.response.data
+        }))
+      } else {
+        return new Promise(resolve => resolve({
+          statusCode: 500,
+          body: 'Erro Inesperado. Tente novamente mais tarde.'
+        }))
+      }
+    }
   }
 }
