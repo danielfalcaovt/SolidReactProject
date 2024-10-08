5c6b41811f8c5f7b7fc221441fa2f107bb3994d4
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-08 14:57:01 -0300

    refactor: move login header and footer to single files

---
+++ src/presentation/components/login-footer/footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as styles from './footer-styles.scss'
+
+export default function Footer (): JSX.Element {
+  return (
+    <footer className={styles.footer} />
+  )
+}
