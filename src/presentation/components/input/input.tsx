1a173b397281faf709ea2a75bf34cdd676492cc7
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-10 15:11:59 -0300

    feat: remove autofill from input

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -4,9 +4,13 @@ import * as styles from './input-styles.scss'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 export default function Input (props: Props): JSX.Element {
+  const autoCompleteRemover = (e: React.FocusEvent<HTMLInputElement>): void => {
+    e.target.readOnly = false
+  }
+
   return (
     <div className={styles.inputWrap}>
-      <input {...props} />
+      <input readOnly onFocus={autoCompleteRemover} {...props} />
       <span>🤬</span>
     </div>
   )
