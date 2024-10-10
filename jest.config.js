6bef20f4335e1dcc115791e53bba4b554ebec403
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-10 15:38:32 -0300

    chore: jsdom and jest babel configuration add

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
+  preset: 'ts-jest',
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
   },
   moduleNameMapper: {
     '@/(.*)': ['<rootDir>/src/$1'],
